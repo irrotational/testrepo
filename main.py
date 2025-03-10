fc33d05d491639548c60cb16fa4179b973803661
irrotational <jwhaleybaldwin@gmail.com>
2025-03-10 16:08:06 +0000

    First commit of this dev branch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utilities import usefulness
 from other_stuff import mystuff
 
+print("OOOooo this is the dev branch")
+
 my_first_thing = usefulness.thing("Paul")
 my_second_thing = usefulness.bigthing("Bob","BigBob")
 mystuff.do_stuff(my_second_thing,"Tony")
